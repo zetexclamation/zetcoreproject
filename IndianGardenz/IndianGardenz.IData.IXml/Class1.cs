a05da5b9a065810d5e1829011bc01612d652753b
zetcore1 <dev.uk.core@gmail.com>
2017-12-19 00:37:47 +0530

    Added two Interface Project for Data and Manager

---
+++ IndianGardenz/IndianGardenz.IData.IXml/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IndianGardenz.IData.IXml
+{
+    public class Class1
+    {
+    }
+}
---
+++ IndianGardenz/IndianGardenz.IManager.IXml/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IndianGardenz.IManager.IXml
+{
+    public class Class1
+    {
+    }
+}
